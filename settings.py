582eb4c6a17f30907de8c7441a560c8e792a9caf
Andrés Martínez <autummata@gmail.com>
2022-10-02 15:30:19 -0700

    minor bug fixes

--- settings.py
+++ settings.py
@@ -1,21 +1,21 @@
 level_map = [  
-'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-'O                H                    D         W      S              N         Y              A                I               Q                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                         x           O',
-'O               xxx   xx                   x  x   xv                xx   v   x         xx   x                                     x    x    xx     fffO',
-'O    P       x                         x               xx                       xx                 x     xx    x     x       xx                       O',
-'O  xxxxx  x                x     xxxx                         xx                              xxx                       x                             O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'O                                                                                                                                                     O',
-'Oxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' ]
+'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
+'O                H                    D         W      S              N               Y              A                I               Q                     O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                         x      xx    x    O',
+'O               xxx   xx                   x  x   xv                xx   v   x         xx   x                                     x    x    xx           fffO',
+'O    P       x                         x               xx                       xx                 x     xx    x     x       xx                             O',
+'O  xxxxx  x                x     xxxx                         xx                              xxx                       x                                   O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'O                                                                                                                                                           O',
+'Oxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' ]
 
 tile_size = 84
 dsp_width = 1280
